5aa1e3e5284c81fc8cf011bb09f4f876a202f97b
JustNoldor <mertsariboga@outlook.com>
2019-03-08 14:17:07 +0300

    #Son.

---
+++ B403Blog/Controllers/PanelProfileController.cs
@@ -0,0 +1,25 @@
+﻿using B403Blog.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B403Blog.Controllers
+{
+    public class PanelProfileController : Controller
+    {
+        BlogYazilimEntities2 db = new BlogYazilimEntities2();
+
+        // GET: PanelProfile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProfilGor(int id)
+        {
+            var data = db.Kullanici.FirstOrDefault(x => x.KullaniciId == id);
+            return View(data);
+        }
+    }
+}
